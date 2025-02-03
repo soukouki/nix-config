6c28bdb54b6b378f493d2b166b614dde71b109f2
soukouki <soukouki0@yahoo.co.jp>
2025-02-04 03:22:20 +0900

    2025-02-04

--- hosts/sou7-proxy/default.nix
+++ hosts/sou7-proxy/default.nix
@@ -77,6 +77,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
